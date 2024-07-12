cb5329ee0b74cccbd813a1c0b73a6ba442448c17
rodolfomori <rod_dolpho@hotmail.com>
2024-07-11 22:28:10 -0300

    Aula 3

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// document = ir no HTML
+// querySelector = busca o que eu quiser que ele busque
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "50%"
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%"
+}
